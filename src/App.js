5ad450f6ac8157bee71e2795a1778d6aaf93c171
MuzamilHassan1 <muzamilhassan1234@gmail.com>
2022-04-05 22:46:07 +0500

    reading data from spacex api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import './App.css';
+import React,{useState,useEffect} from "react";
+const QUERY = `
+{
+  launchesPast {
+		id
+    mission_name
+    launch_date_local
+    ships {
+      name
+    }
+  }
+}
+`
 
 function App() {
+
+  const [launches,setLaunches] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://api.spacex.land/graphql/',{
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: QUERY})
+    }).then(resp => resp.json())
+    .then(data => setLaunches(data.data.launchesPast))
+  },[]);
+
   return (
-    <div>HELLO WORLD</div>
+    <div>
+      {
+        launches.map(l=>(
+          <li>{l.id +": "+ l.mission_name}</li>
+        ))
+      }
+
+    </div>
   );
 }
 
