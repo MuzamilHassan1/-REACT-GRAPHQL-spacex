6a9c6b5d1f2e8e7a39ad55efe72eb052ad620830
MuzamilHassan1 <muzamilhassan1234@gmail.com>
2022-04-06 17:37:39 +0500

    scrolling effect in pagination

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import CardList from './CardList';
 
 const Pagination = ({ data, dataLimit }) => {
   const [pages] = useState(Math.round(data.length / dataLimit));
   const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    window.scrollTo({ behavior: 'smooth', top: '0px' });
+  }, [currentPage]);
 
   const goToNextPage = () => {
     setCurrentPage(currentPage + 1);
